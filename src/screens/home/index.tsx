a9fb0ed1961430a6324f3708946a156fb4e46f8c
Siddhartha <siddureddyvr1115@email.com>
2026-03-08 17:27:17 +0530

    Added basic draw function

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,49 @@
+// Functional Component and Class Components are 2 react components
+// Ref is used to store a value in react component but not re-render the component
+
+import { useState, useRef } from "react";
+
+export default function Home() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if(canvas){
+            canvas.style.background = 'black';
+            const ctx = canvas.getContext('2d');
+            if(ctx){
+                ctx.beginPath();
+                ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                setIsDrawing(true);
+            }
+        }
+    }
+    
+    const stopDrawing = () => {
+        setIsDrawing(false);
+    }
+
+    const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        if(!isDrawing){
+            return;
+        }
+        const canvas = canvasRef.current;
+        if(canvas){
+            const ctx = canvas.getContext('2d');
+            if(ctx){
+                ctx.strokeStyle = 'white';
+                ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                ctx.stroke();
+            }
+        }
+    }
+  return (
+    <canvas
+      ref = {canvasRef}
+      id = 'canvas'
+      className="absolute top-0 left-0 w-full h-full"
+      onMouseDown = {startDrawing}
+      />
+  );
+}
